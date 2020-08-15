c6bd7a7525f5b88ded27b20bec6350bc9809976b
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-15 05:20:49 +0000

    addmain

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.23.0"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
 
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+}
 
