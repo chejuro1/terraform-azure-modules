ef986f0cc2f16b9e8dfbd222c0c69b96ff47e2b9
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-18 04:36:47 +0000

    add version

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "rg" {
 #Create Storage Account
 module "storage_account" {
   source    = "./modules/storage-account"
-
+  version   = "0.0.5"
   saname    = "statfdemosa234"
   rgname    = azurerm_resource_group.rg.name
   location  = azurerm_resource_group.rg.location
